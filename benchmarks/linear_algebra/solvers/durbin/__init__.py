0143240604aa4e76105ef7b173deba24f404d0df
Gabriel <grodriguez@udc.es>
2021-01-27 10:37:20 +0100

    Modified license notes.

--- benchmarks/linear_algebra/solvers/durbin/__init__.py
+++ benchmarks/linear_algebra/solvers/durbin/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Miguel Angel Abella Gonzalez <miguel.abella@udc.es>
+# Copyright 2021 Universidade da Coruña
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
