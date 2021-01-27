3e66529ba3c8ad7359c8ea1cfc5747c414fca44b
Gabriel <grodriguez@udc.es>
2021-01-27 10:42:41 +0100

    Added author and contact to all files.

--- benchmarks/datamining/__init__.py
+++ benchmarks/datamining/__init__.py
@@ -11,5 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Author:
+#    Miguel Ángel Abella González <miguel.abella@udc.es>
+#
+# Contact:
+#    Gabriel Rodríguez <gabriel.rodriguez@udc.es>
+
 
 """Leave this file empty."""
--- benchmarks/stencils/fdtd_2d/__init__.py
+++ benchmarks/stencils/fdtd_2d/__init__.py
@@ -11,6 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Author:
+#    Miguel Ángel Abella González <miguel.abella@udc.es>
+#
+# Contact:
+#    Gabriel Rodríguez <gabriel.rodriguez@udc.es>
+
 
 """Leave this file's content empty if it resides in any intermediate package directory.
 When this file is in a benchmark folder, include the package and the benchmark.
